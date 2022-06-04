ab5b18d5c48018097538032289b5d4a7be33c6bf
Ruizhe Wang <rw465@cornell.edu>
2022-06-03 20:41:25 -0400

    add vlog

--- travel/shuffle.py
+++ travel/shuffle.py
@@ -1,19 +1,24 @@
 import os
-# from glob import glob
+import sys
+from glob import glob
 import re
 from numpy.random import choice
-# jpgs = glob("*.jpg")
 
-print(os.listdir('.'))
+cwd = os.getcwd()
 
-files = [f for f in os.listdir('.') if re.match(r'^[0-9]*(.jpg|.JPG)', f)]
-# lst = choice(range(1, len(files)+1), len(files), replace=False)
+for dir in sys.argv[1:]:
+    os.chdir(dir+"/images")
+    jpgs = glob('*.jpg')+glob('*.JPG')+glob('*.jpeg')
+    jpgs = [jpg for jpg in jpgs if jpg != "overlay.jpg" and jpg != "overlay.JPG"]
+    # files = [f for f in os.listdir('.') if re.match(r'^[0-9]*(.jpg|.JPG)', f)]
+    lst = choice(range(1, len(jpgs)+1), len(jpgs), replace=False)
 
-# for f, num in zip(files, lst):
-#     os.rename(f, str(10000+num)+".jpg")
+    for f, num in zip(jpgs, lst):
+        os.rename(f, str(10000+num)+".jpg")
 
-# files = [f for f in os.listdir('.') if re.match(r'^[0-9]*(.jpg|.JPG)', f)]
-# lst = choice(range(1, len(files)+1), len(files), replace=False)
+    jpgs = glob('*.jpg')
+    jpgs = [jpg for jpg in jpgs if jpg!="overlay.jpg"]
 
-# for f in files:
-#     os.rename(f, str(int(f[:5])-10000)+".jpg")
+    for f in jpgs:
+        os.rename(f, str(int(f[:5])-10000)+".jpg")
+    os.chdir(cwd)
